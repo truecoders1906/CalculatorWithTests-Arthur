b1d9abd40dd508e0ace6511933688c74481bbf4e
Arthur Triantos <arttriantos@gmail.com>
2019-07-10 12:49:45 -0500

    Wrote subtraction test.

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -8,10 +8,27 @@ namespace CalcTest
     {
         [Theory]
         [InlineData(2,3,5)]
+        [InlineData(1,1,2)]
+        [InlineData(10,14,24)]
+        [InlineData(-12,11,-1)]
+        [InlineData(9000,1,9001)]
         public void AddTwoNumbers(int num1, int num2, int expectedSum)
         {
             Calculator challenger = new Calculator();
             int actual = challenger.AddTwoNumbers(num1, num2);
+            Assert.Equal(expectedSum, actual);
+        }
+
+        [Theory]
+        [InlineData(15,7,8)]
+        [InlineData(1,6,-5)]
+        [InlineData(11.7,9.2,2.5)]
+        [InlineData(1000,500,500)]
+        [InlineData(26,39,-13)]
+        public void SubtractTwoNumbers(double minuend, double subtrahend, double expectedSum)
+        {
+            Calculator challenger = new Calculator();
+            double actual = challenger.SubtractTwoNumbers(minuend, subtrahend);
             Assert.Equal(expectedSum, actual);
         }
     }
