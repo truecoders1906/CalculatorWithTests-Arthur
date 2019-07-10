ab1dc6946ddd9eef193a137ce1046f1b4e99c371
Arthur Triantos <arttriantos@gmail.com>
2019-07-10 11:17:19 -0500

    Created the 5 methods in Calculator class.

--- CalculatorWithTests/Calculator.cs
+++ CalculatorWithTests/Calculator.cs
@@ -3,6 +3,29 @@ namespace CalculatorWithTests
 {
     class Calculator
     {
-        
+        public int AddTwoNumbers(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double SubtractTwoNumbers(double minuend, double subtrahend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long MultiplyTwoNumbers(long factor1, long factor2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal DivideTwoNumbers(decimal denominator, decimal numerator)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double FindTheFactorial(int number)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
